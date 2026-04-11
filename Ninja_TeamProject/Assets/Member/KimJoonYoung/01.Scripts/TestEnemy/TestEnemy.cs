cde714fd753596dde23da99f53614f13c5b342e1
kjy08136 <kimj08136@gmail.com>
2026-04-11 17:42:49 +0900

    HP Text 추가 (해봣음)

--- Ninja_TeamProject/Assets/Member/KimJoonYoung/01.Scripts/TestEnemy/TestEnemy.cs
+++ Ninja_TeamProject/Assets/Member/KimJoonYoung/01.Scripts/TestEnemy/TestEnemy.cs
@@ -1,19 +1,37 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class TestEnemy : Agent
 {
+    [SerializeField] private TextMeshProUGUI hpText;
+    [SerializeField] private int _testenemyHP = 10;
+
     private bool attacked;
 
+    // Hash
     private int _isAttackedHash = Animator.StringToHash("Attacked");
+
+    private void Start()
+    {
+        hpText.text = ($"Enemy Bot HP : {_testenemyHP}");
+    }
 
     private void Update()
     {
         _agentRenderer.SetBoolParam(_isAttackedHash, attacked);
+
+        if (_testenemyHP < 1)
+        {
+            hpText.text = ($"Died");
+            Destroy(gameObject);
+        }
     }
 
     public void AttackedNow(bool atk)
     {
+        _testenemyHP--;
+        hpText.text = ($"Enemy Bot HP : {_testenemyHP}");
         attacked = atk;
         StartCoroutine(Attacked());
     }
