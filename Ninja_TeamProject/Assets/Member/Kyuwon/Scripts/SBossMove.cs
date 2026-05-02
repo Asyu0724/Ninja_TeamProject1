20b998131404744f9bf61d73234663c0f57b3862
SIMkyuwon <kgw0809@kakao.com>
2026-05-02 10:41:43 +0900

    boss moving

--- Ninja_TeamProject/Assets/Member/Kyuwon/Scripts/SBossMove.cs
+++ Ninja_TeamProject/Assets/Member/Kyuwon/Scripts/SBossMove.cs
@@ -4,20 +4,37 @@ using UnityEngine.EventSystems;
 
 public class SBossMove : MonoBehaviour
 {
-    [SerializeField]private float speed = 4.6f;
+    private float speed = 3.0f;
     [SerializeField]private Rigidbody2D rigid;
     [SerializeField]private Transform playerTRM;
     [SerializeField]private Vector2 moveDir;
 
+
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        playerTRM = GameObject.Find("Square").transform;
     }
 
     private void FixedUpdate()
     {
-        playerTRM = GameObject.Find("Square").transform;
-        moveDir = (playerTRM.position - transform.position).normalized;
-        rigid.linearVelocityX = moveDir.x * speed;
+        if (playerTRM != null)
+        {
+            moveDir = (playerTRM.position - transform.position).normalized;
+            rigid.linearVelocityX = moveDir.x * speed;
+            if (moveDir.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if (moveDir.x < 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        
     }
 }
