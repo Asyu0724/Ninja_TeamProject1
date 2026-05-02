d17b6307f2bb6da5238a8bbe77e8c3f2c8f6a45c
SIMkyuwon <kgw0809@kakao.com>
2026-05-02 13:36:31 +0900

    보스 기본 공격 미완

--- Ninja_TeamProject/Assets/Member/Kyuwon/Scripts/SBossMove.cs
+++ Ninja_TeamProject/Assets/Member/Kyuwon/Scripts/SBossMove.cs
@@ -1,19 +1,22 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
 
 public class SBossMove : MonoBehaviour
 {
     private float speed = 3.0f;
     [SerializeField]private Rigidbody2D rigid;
     [SerializeField]private Transform playerTRM;
-    [SerializeField]private Vector2 moveDir;
-
+    [SerializeField] private Vector2 moveDir;
+    [SerializeField] private Animator animator;
+    private bool isAttacking = false;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         playerTRM = GameObject.Find("Square").transform;
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void FixedUpdate()
@@ -30,11 +33,31 @@ public class SBossMove : MonoBehaviour
             {
                 transform.rotation = Quaternion.Euler(0, 180, 0);
             }
+
+            int BossMove = Mathf.Abs(moveDir.x) >= 0.1f ? 1 : 0;
+            animator.SetFloat("MoveX", BossMove);
         }
     }
 
-    private void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if(collision.gameObject.CompareTag("UMM") && !isAttacking)
+        {
+            Debug.Log("sj");
+            StartCoroutine(AttackRoutine());
+        }
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        isAttacking = true;
+
+        animator.SetTrigger("Slash");
+
+        yield return new WaitForSeconds(1.0f);
+
+        isAttacking = false;
+
+        rigid.linearVelocityX = 3.0f;
     }
 }
