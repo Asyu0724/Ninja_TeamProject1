08c7b816b226544df71a01e240b908451615dce3
SIMkyuwon <kgw0809@kakao.com>
2026-05-13 20:25:49 +0900

    etc

--- Ninja_TeamProject/Assets/Member/Kyuwon/Scripts/BossCharge.cs
+++ Ninja_TeamProject/Assets/Member/Kyuwon/Scripts/BossCharge.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using Member.Kyuwon.SBossSO;
 
 public class BossCharge : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public SBossData bossData;
+    
+    private Animator _animator;
+    [SerializeField] private LayerMask whatIsPlayer;
+    
+    private void Awake()
     {
-        
+        _animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Charge()
     {
-        
+        Collider2D isHit = Physics2D.OverlapBox(transform.position, bossData.ChargeRange, 0,whatIsPlayer);
+
+        if (isHit != null)
+        {
+            float timeStamp = Time.time;
+            
+            _animator.SetTrigger("Charger");
+        }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cornflowerBlue;
+        Gizmos.DrawWireCube(transform.position, bossData.ChargeRange);
     }
 }
