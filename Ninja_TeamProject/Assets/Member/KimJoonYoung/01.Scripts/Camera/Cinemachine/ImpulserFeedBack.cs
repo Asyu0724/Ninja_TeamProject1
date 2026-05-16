de9840747e3b55022e59445e069c4eba79145d1c
kjy08136 <kimj08136@gmail.com>
2026-05-16 15:36:44 +0900

    ++

--- Ninja_TeamProject/Assets/Member/KimJoonYoung/01.Scripts/Camera/Cinemachine/ImpulserFeedBack.cs
+++ Ninja_TeamProject/Assets/Member/KimJoonYoung/01.Scripts/Camera/Cinemachine/ImpulserFeedBack.cs
@@ -1,17 +1,27 @@
 using System;
 using Unity.Cinemachine;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Member.KimJoonYoung._01.Scripts.Camera.Cinemachine
 {
     public class ImpulserFeedBack : MonoBehaviour
     {
-        [SerializeField] private Vector3 velocity;
+        [SerializeField] private float sec;
+        [SerializeField] private Vector3 velocityRange1;
+        [SerializeField] private Vector3 velocityRange2;
         [SerializeField] private CinemachineImpulseSource impulser;
 
         public void Impulse()
         {
-            impulser.GenerateImpulse(velocity);
+            impulser.DefaultVelocity = new Vector3(
+                Random.Range(velocityRange1.x, velocityRange2.x)
+                , Random.Range(velocityRange1.y, velocityRange2.y)
+                , Random.Range(velocityRange1.z, velocityRange2.z));
+            
+            impulser.ImpulseDefinition.ImpulseShape = CinemachineImpulseDefinition.ImpulseShapes.Bump;
+            impulser.ImpulseDefinition.ImpactRadius = sec;
+            impulser.GenerateImpulse();
         }
     }
 }
--- Ninja_TeamProject/Assets/Member/KimJoonYoung/01.Scripts/Effect/InPortalScene.cs
+++ Ninja_TeamProject/Assets/Member/KimJoonYoung/01.Scripts/Effect/InPortalScene.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using Member.KimJoonYoung._01.Scripts.UI.Portal;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Member.KimJoonYoung._01.Scripts.Effect
@@ -8,6 +9,8 @@ namespace Member.KimJoonYoung._01.Scripts.Effect
     public class InPortalScene : MonoBehaviour
     {
         [SerializeField] private Image blackScreenImage;
+        public UnityEvent OnEvent;
+
         private void Start()
         {
             PortalButtonAction.Instance.OnButtonPress += HandlerChangeTransform;
@@ -19,7 +22,13 @@ namespace Member.KimJoonYoung._01.Scripts.Effect
             seq.SetUpdate(true);
             seq.PrependInterval(1f);
             seq.Prepend(transform.DOScale(new Vector3(2f, 2f, -2), 1f).SetEase(Ease.OutCubic));
+            seq.OnPlay(Event);
             seq.Append(blackScreenImage.DOFade(1f, 3f).SetEase(Ease.InOutCubic));
         }
-    }
+
+        private void Event()
+        {
+            OnEvent?.Invoke();
+        }
+}
 }
