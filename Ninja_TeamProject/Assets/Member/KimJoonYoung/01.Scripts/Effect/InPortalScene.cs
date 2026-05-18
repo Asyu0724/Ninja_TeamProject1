6b2e3cec7662044944f9aaf3a1ed99ef83213779
kjy08136 <kimj08136@gmail.com>
2026-05-18 21:12:34 +0900

    Revert "SpiderBoss+"
    This reverts commit c96326c36e59205027f1aa145b2be205ebb338d1.

--- Ninja_TeamProject/Assets/Member/KimJoonYoung/01.Scripts/Effect/InPortalScene.cs
+++ Ninja_TeamProject/Assets/Member/KimJoonYoung/01.Scripts/Effect/InPortalScene.cs
@@ -1,19 +1,15 @@
-    using System;
     using DG.Tweening;
-    using Member.KimJoonYoung._01.Scripts.UI;
-    using Member.KimJoonYoung._01.Scripts.UI.Portal;
+using Member.KimJoonYoung._01.Scripts.UI.Portal;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Member.KimJoonYoung._01.Scripts.Effect
 {
     public class InPortalScene : MonoBehaviour
     {
-        [SerializeField] private GameObject sceneChange;
         [SerializeField] private Image blackScreenImage;
-        public UnityEvent onEvent;
+        public UnityEvent OnEvent;
 
         private void Start()
         {
@@ -28,19 +24,17 @@ namespace Member.KimJoonYoung._01.Scripts.Effect
             seq.Prepend(transform.DOScale(new Vector3(2f, 2f, -2), 1f).SetEase(Ease.OutCubic));
             seq.OnPlay(Event);
             seq.Append(blackScreenImage.DOColor(Color.green,0).SetEase(Ease.OutCubic));
-            seq.AppendInterval(0.05f);
+            seq.AppendInterval(0.1f);
             seq.Append(blackScreenImage.DOColor(Color.deepPink,0).SetEase(Ease.OutCubic));
             seq.AppendInterval(0.05f);
             seq.Append(blackScreenImage.DOColor(Color.white, 0).SetEase(Ease.OutCubic));
-            seq.AppendInterval(0.05f);
+            seq.AppendInterval(0.1f);
             seq.Append(blackScreenImage.DOColor(Color.black, 0).SetEase(Ease.OutCubic));
-            seq.AppendInterval(1.5f);
-            seq.OnComplete(sceneChange.GetComponent<SceneChangeManager>().ChangeScene);
         }
 
         private void Event()
         {
-            onEvent?.Invoke();
+            OnEvent?.Invoke();
         }
 }
 }
